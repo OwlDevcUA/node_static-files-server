57758b4b83a56f0d565b3b9e4dfe71b43103f057
Kirill <kkoctuk2005@gmail.com>
2026-07-15 18:54:00 +0200

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -3,12 +3,16 @@
 const http = require('node:http');
 const path = require('node:path');
 const fsp = require('node:fs/promises');
+const publicFolderPath = path.resolve(__dirname, '..', 'public');
 
 function createServer() {
   const server = http.createServer(async (req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
 
-    if (req.url.includes('../')) {
+    if (
+      req.url.includes('../') ||
+      (!url.pathname.startsWith('/file/') && path.extname(url.pathname))
+    ) {
       res.statusCode = 400;
       res.setHeader('Content-Type', 'text/plain');
 
@@ -42,11 +46,18 @@ function createServer() {
       filePath = 'index.html';
     }
 
-    const realPath = path.join(__dirname, '..', 'public', filePath);
+    const realPath = path.resolve(publicFolderPath, filePath);
+    const relativePath = path.relative(publicFolderPath, realPath);
+
+    if (relativePath.startsWith('..') || path.isAbsolute(relativePath)) {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/plain');
+
+      return res.end('Bad request');
+    }
 
     try {
       const file = await fsp.readFile(realPath, 'utf-8');
-
       const ext = path.extname(realPath);
       let contentType = 'text/plain';
 
@@ -55,7 +66,6 @@ function createServer() {
       } else if (ext === '.css') {
         contentType = 'text/css';
       }
-
       res.statusCode = 200;
       res.setHeader('Content-Type', contentType);
       res.end(file);
